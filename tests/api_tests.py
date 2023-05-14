1034474ce7cc8ec3758f2876af9790be8283666c
koobalack <koobalack@gmail.com>
2023-05-15 01:10:29 +0300

    Removed unused imports

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -1,6 +1,5 @@
 import pytest
 import json
-from types import SimpleNamespace
 
 from starlette.testclient import TestClient
 from main import app as application
@@ -8,6 +7,7 @@ from api.v1.models.pong import Pong
 
 client = TestClient(application)
 
+pong = Pong()
 
 @pytest.mark.webtest
 def test_ping():
@@ -15,4 +15,5 @@ def test_ping():
     data = json.loads(response.content)
 
     assert response.status_code == 200
-    assert data['description'] == "I'm alive"
+    assert data['description'] == pong.description
+
